35980cb75279edac9f79879890cb8147fa8ea5f9
Zachary Nicol-P <1735193@cegep-ste-foy.qc.ca>
2019-10-09 16:10:09 -0400

    catalogue

--- app/Controleurs/ControleurLivre.php
+++ app/Controleurs/ControleurLivre.php
@@ -15,41 +15,59 @@ class ControleurLivre
 {
     private $blade = null;
 
-    public function __construct(){
-        $this->blade = App::getInstance() -> getBlade();
+    public function __construct()
+    {
+        $this->blade = App::getInstance()->getBlade();
     }
 
     /**s
      * Fonction index qui call la view
      */
-    public function catalogue(): void{
-        $tDonnees = array_merge($this -> getDonneesLivres(), ControleurSite::getDonneeFragmentPiedDePage());;
+    public function catalogue(): void
+    {
+        $tDonnees = array_merge($this->getDonneesLivres(), ControleurSite::getDonneeFragmentPiedDePage());;
         echo $this->blade->run("livres.catalogue", $tDonnees);
     }
 
-    public function fiche():void{
-        $tDonnees = array_merge($this -> getDonneesUnLivre(), ControleurSite::getDonneeFragmentPiedDePage());;
+    public function fiche(): void
+    {
+        $tDonnees = array_merge($this->getDonneesUnLivre(), ControleurSite::getDonneeFragmentPiedDePage());;
         echo $this->blade->run("livres.fiche", $tDonnees);
     }
 
     /**
      * @return array
      */
-    public function getDonneesLivres():array{
+    public function getDonneesLivres(): array
+    {
         /**
          * Définition du nombre de pages
          */
         $nbrLivres = Livre::compter();
-        $livresParPage = 9;
-        $nombreTotalPages = ceil($nbrLivres/$livresParPage);
+
+        if (isset($_GET["nbParPages"])) {
+            if ($_GET["nbParPages"] == '9') {
+                $livresParPage = 9;
+            }
+            if ($_GET["nbParPages"] == '36') {
+                $livresParPage = 36;
+            }
+            if ($_GET["nbParPages"] == '72') {
+                $livresParPage = 72;
+            }
+        } else {
+            $livresParPage = 9;
+        }
+
+
+        $nombreTotalPages = ceil($nbrLivres / $livresParPage);
 
         /**
          * Définition de la page courante
          */
-        if(isset($_GET["page"])){
+        if (isset($_GET["page"])) {
             $numeroPage = $_GET["page"];
-        }
-        else{
+        } else {
             $numeroPage = 1;
         }
 
@@ -62,10 +80,10 @@ class ControleurLivre
         /**
          * Définition du array de données à envoyer dans la page
          */
-        $arrLivres = Livre::trouverParLimite(intval($numeroPage) -1 , $livresParPage);
+        $arrLivres = Livre::trouverParLimite(intval($numeroPage) - 1, $livresParPage);
 
-        foreach($arrLivres as $livre){
-            $livre -> isbn13 = Livre::ISBNToEAN($livre -> isbn);
+        foreach ($arrLivres as $livre) {
+            $livre->isbn13 = Livre::ISBNToEAN($livre->isbn);
         }
 
         $arrCategories = Categories::trouverTout();
@@ -83,31 +101,32 @@ class ControleurLivre
         return $arrDonnees;
     }
 
-    public function getDonneesUnLivre():array{
+    public function getDonneesUnLivre(): array
+    {
         $isbnLivre = "0";
-        if(isset($_GET["isbn"])){
+        if (isset($_GET["isbn"])) {
             $isbnLivre = $_GET["isbn"];
         }
 
         $infosLivre = Livre::trouverParIsbn($isbnLivre);
 
-        $infosLivre -> __set("isbn13", Livre::ISBNToEAN($infosLivre -> __get("isbn")));
+        $infosLivre->__set("isbn13", Livre::ISBNToEAN($infosLivre->__get("isbn")));
 
-        $arrRecensions = Recension::trouverRecensions($infosLivre -> __get("id"));
-        foreach($arrRecensions as $rescension){
-            $rescension -> description = Util::couperParagraphe($rescension -> description);
-            $rescension -> date = new DateTime($rescension -> date);
-            $rescension -> date -> format("d M Y");
+        $arrRecensions = Recension::trouverRecensions($infosLivre->__get("id"));
+        foreach ($arrRecensions as $rescension) {
+            $rescension->description = Util::couperParagraphe($rescension->description);
+            $rescension->date = new DateTime($rescension->date);
+            $rescension->date->format("d M Y");
         }
 
         //Honneurs
-        $arrHonneurs = Honneur::trouverHonneursLivre($infosLivre -> __get("id"));
-        foreach($arrHonneurs as $honneur){
-            $honneur -> description = Util::couperParagraphe($honneur -> description);
+        $arrHonneurs = Honneur::trouverHonneursLivre($infosLivre->__get("id"));
+        foreach ($arrHonneurs as $honneur) {
+            $honneur->description = Util::couperParagraphe($honneur->description);
         }
         $arrInfos = array_merge(array("livre" => $infosLivre),
-                                array("arrRecensions" => $arrRecensions),
-                                array("arrHonneurs" => $arrHonneurs));
+            array("arrRecensions" => $arrRecensions),
+            array("arrHonneurs" => $arrHonneurs));
         return $arrInfos;
     }
 }
