3557a451e967d44385d9f736c622add800f8de47
Zachary Nicol-P <1735193@cegep-ste-foy.qc.ca>
2019-10-11 14:56:59 -0400

    update CSS catalogue

--- ressources/vues/livres/catalogue.blade.php
+++ ressources/vues/livres/catalogue.blade.php
@@ -1,100 +1,106 @@
 @extends('gabarit')
 
 @section('contenu')
-    <main class="container col-12">
-        <div class="row">
-            <div class="row dropdown">
-                <button onclick="ouvrirFermerCategories()" class="align-self-center dropbtn catalogue__btn--categories">
-                    CATÉGORIES
-                </button>
-                <div id="myDropdown" class="dropdown-content">
-                    <form method="post" action="{{$urlPagination}}">
-                        <a href="index.php?controleur=livre&action=catalogue&categorie=0&trierPar={{$_GET['trierPar']}}&nbParPages={{ $_GET['nbParPages'] }}"
-                           class="catalogue__btn catalogue__btn--reinitialiser row justify-content-center">RÉINITIALISER</a>
-                        <ul class="catalogue__categories__liste">
-                            @foreach($arrCategories as $categorie)
-                                <li class="catalogue__categories__liste__item">
-                                    <a href="index.php?controleur=livre&action=catalogue&categorie={{$categorie -> id}}&trierPar={{$_GET['trierPar']}}&nbParPages={{ $_GET['nbParPages'] }}" class="catalogue__categories__lien">{{$categorie -> nom_fr}}</a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </form>
-                </div>
+    <div class="row">
+        <div class="row dropdown">
+            <button onclick="ouvrirFermerCategories()" class="dropbtn catalogue__btn--categories ">
+                CATÉGORIES
+            </button>
+            <div id="myDropdown" class="dropdown-content justify-content-center">
+                <form method="post" action="{{$urlPagination}}">
+                    <a href="index.php?controleur=livre&action=catalogue&categorie=0&trierPar={{$_GET['trierPar']}}&nbParPages={{ $_GET['nbParPages'] }}"
+                       class="catalogue__btn catalogue__btn--reinitialiser">RÉINITIALISER</a>
+                    <ul class="catalogue__categories__liste">
+                        @foreach($arrCategories as $categorie)
+                            <li class="catalogue__categories__liste__item">
+                                <a href="index.php?controleur=livre&action=catalogue&categorie={{$categorie -> id}}&trierPar={{$_GET['trierPar']}}&nbParPages={{ $_GET['nbParPages'] }}"
+                                   class="catalogue__categories__lien">{{$categorie -> nom_fr}}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </form>
             </div>
-            <div class="catalogue__categories col-md-3">
-                <p class="catalogue__categories__titre text-center">
-                    CATÉGORIES
-                </p>
+        </div>
+        <div class="catalogue__categories col-lg-3">
+            <p class="catalogue__categories__titre text-center">
+                CATÉGORIES
+            </p>
+            <div class="catalogue__categories__contenu">
                 <a href="index.php?controleur=livre&action=catalogue&categorie=0&trierPar={{$_GET['trierPar']}}&nbParPages={{ $_GET['nbParPages'] }}"
-                        class="catalogue__btn catalogue__categories--reinitialiser">RÉINITIALISER</a>
-                <ul class="catalogue__categories__liste">
-                    @foreach($arrCategories as $categorie)
-                        <li class="catalogue__categories__liste__item">
-                            <a href="index.php?controleur=livre&action=catalogue&categorie={{$categorie -> id}}&trierPar={{$_GET['trierPar']}}&nbParPages={{ $_GET['nbParPages'] }}" class="catalogue__categories__lien">{{$categorie -> nom_fr}}</a>
-                        </li>
-                    @endforeach
-                </ul>
+                   class="catalogue__btn catalogue__categories--reinitialiser">RÉINITIALISER</a>
+            <ul class="catalogue__categories__liste">
+                @foreach($arrCategories as $categorie)
+                    <li class="catalogue__categories__liste__item">
+                        <a href="index.php?controleur=livre&action=catalogue&categorie={{$categorie -> id}}&trierPar={{$_GET['trierPar']}}&nbParPages={{ $_GET['nbParPages'] }}"
+                           class="catalogue__categories__lien">{{$categorie -> nom_fr}}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+        </div>
+        <div class="col-lg-9 justify-content-center">
+            <div class="catalogue__nbParPages">
+                <input type="text" class="catalogue__rechercher">
+                <input type="submit" class="catalogue__rechercher catalogue__rechercher__bouton" value="Rechercher">
+                <div class="row">
+                    <p class="catalogue__nbParPages__label">Nombre de livres par pages :</p>
+                    <a class="catalogue__lien catalogue__lien"
+                       href="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar={{$_GET['trierPar']}}&nbParPages=9">9</a>
+                    <a class="catalogue__lien"
+                       href="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar={{$_GET['trierPar']}}&nbParPages=18">18</a>
+                    <a class="catalogue__lien"
+                       href="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar={{$_GET['trierPar']}}&nbParPages=36">36</a>
+                </div>
             </div>
-            <div class="col-md-9 catalogue__contenu">
-                <div class="catalogue__rangee--1">
-                    <input type="text" class="catalogue__rechercher">
-                    <input type="submit" class="catalogue__rechercher catalogue__rechercher__bouton" value="Rechercher">
-                    <div class="row catalogue__nbParPages">
-                        <p class="catalogue__nbParPages__label">Nombre de livres par pages :</p>
-                        <a class="catalogue__lien catalogue__lien"
-                           href="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar={{$_GET['trierPar']}}&nbParPages=9">9</a>
-                        <a class="catalogue__lien"
-                           href="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar={{$_GET['trierPar']}}&nbParPages=18">18</a>
-                        <a class="catalogue__lien"
-                           href="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar={{$_GET['trierPar']}}&nbParPages=36">36</a>
-                    </div>
-                </div>
-                <div class="row catalogue__rangee--2">
-                    {{--                    <p class="col-sm-12 col-md- catalogue__nbResultatsRecherche">*Nombre de livres trouvées*</p>--}}
-                    <label for="catalogue__trier">Trier par :</label>
-                    <select class="catalogue__trier" id="catalogue__trier" onchange=" location = this.value">
-                        <option value="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar=aucun&nbParPages={{$_GET['nbParPages']}}"
-                                @if(isset($_GET["trierPar"])== 'aucun' OR !isset($_GET["trierPar"]))
-                                selected
-                                @endif
-                        >Aucun
-                        </option>
-                        <option value="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar=alphabetique&nbParPages={{$_GET['nbParPages']}}"
-                                @if(isset($_GET["trierPar"]) AND $_GET["trierPar"] == 'alphabetique')
-                                selected
-                                @endif>Ordre alphabétique
-                        </option>
-                        <option value="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar=prixCroissant&nbParPages={{$_GET['nbParPages']}}"
-                                @if(isset($_GET["trierPar"]) AND $_GET["trierPar"] == 'prixCroissant')
-                                selected
-                                @endif>Prix: $ -> $$$
-                        </option>
-                        <option value="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar=prixDecroissant&nbParPages={{$_GET['nbParPages']}}"
-                                @if(isset($_GET["trierPar"]) AND $_GET["trierPar"] == 'prixDecroissant')
-                                selected
-                                @endif
-                        >Prix: $$$ -> $
-                        </option>
-                    </select>
-{{--                    <div class="catalogue__affichage">--}}
-{{--                        <p class="catalogue__affichage__label">Affichage :</p>--}}
-{{--                        <a class="catalogue__affichage__icone--actif"--}}
-{{--                           href="index.php?controleur=livre&action=catalogue&affichage=rangees">Rangées</a>--}}
-{{--                        <a class="catalogue__affichage__icone"--}}
-{{--                           href="index.php?controleur=livre&action=catalogue&affichage=cartes">Cartes</a>--}}
-{{--                    </div>--}}
-                </div>
-                <div class="catalogue__lesLivres row">
-                    @foreach($arrLivres as $livre)
+            <div class="row catalogue__trier">
+                {{--                    <p class="col-sm-12 col-md- catalogue__nbResultatsRecherche">*Nombre de livres trouvées*</p>--}}
+                <label for="catalogue__trier">Trier par :</label>
+                <select class="catalogue__trier--select" id="catalogue__trier" onchange=" location = this.value">
+                    <option value="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar=aucun&nbParPages={{$_GET['nbParPages']}}"
+                            @if(isset($_GET["trierPar"])== 'aucun' OR !isset($_GET["trierPar"]))
+                            selected
+                            @endif
+                    >Aucun
+                    </option>
+                    <option value="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar=alphabetique&nbParPages={{$_GET['nbParPages']}}"
+                            @if(isset($_GET["trierPar"]) AND $_GET["trierPar"] == 'alphabetique')
+                            selected
+                            @endif>Ordre alphabétique
+                    </option>
+                    <option value="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar=prixCroissant&nbParPages={{$_GET['nbParPages']}}"
+                            @if(isset($_GET["trierPar"]) AND $_GET["trierPar"] == 'prixCroissant')
+                            selected
+                            @endif>Prix: $ -> $$$
+                    </option>
+                    <option value="index.php?controleur=livre&action=catalogue&categorie={{$_GET['categorie']}}&trierPar=prixDecroissant&nbParPages={{$_GET['nbParPages']}}"
+                            @if(isset($_GET["trierPar"]) AND $_GET["trierPar"] == 'prixDecroissant')
+                            selected
+                            @endif
+                    >Prix: $$$ -> $
+                    </option>
+                </select>
+                {{--                    <div class="catalogue__affichage">--}}
+                {{--                        <p class="catalogue__affichage__label">Affichage :</p>--}}
+                {{--                        <a class="catalogue__affichage__icone--actif"--}}
+                {{--                           href="index.php?controleur=livre&action=catalogue&affichage=rangees">Rangées</a>--}}
+                {{--                        <a class="catalogue__affichage__icone"--}}
+                {{--                           href="index.php?controleur=livre&action=catalogue&affichage=cartes">Cartes</a>--}}
+                {{--                    </div>--}}
+            </div>
+            <div class="row">
+                @foreach($arrLivres as $livre)
+                    <div class="catalogue__lesLivres col-md-6 col-lg-4">
                         <img src="{{ $livre -> getImageUrl() }}" alt="Couverture du livre {{ $livre -> titre }}"
                              class="catalogue__couverture">
                         <div class="catalogue__livre">
                             <div class="catalogue__livre__titreEtAuteur">
                                 <a class="catalogue__titre"
                                    href="index.php?controleur=livre&action=fiche&isbn={{ $livre -> isbn }}">{{ $livre -> titre }}</a>
-                                @foreach($livre -> getAuteurs() as $auteur)
-                                    <p class="catalogue__auteurs">{{ $auteur -> getNomPrenom() }}</p>
-                                @endforeach
+                                <div class="row">
+                                    @foreach($livre -> getAuteurs() as $auteur)
+                                        <p class="catalogue__auteurs">{{ $auteur -> getNomPrenom() }}</p>
+                                    @endforeach
+                                </div>
                                 <p class="catalogue__prix">{{$livre-> prix}}$</p>
                             </div>
                             <div class="catalogue__livre__boutons"><a
@@ -105,12 +111,12 @@
                             </div>
 
                         </div>
-                    @endforeach
-                </div>
-                @include("livres.fragments.pagination")
+                    </div>
+                @endforeach
             </div>
+            @include("livres.fragments.pagination")
         </div>
-    </main>
+    </div>
     <script>
         function ouvrirFermerCategories() {
             document.getElementById("myDropdown").classList.toggle("show");
@@ -129,6 +135,5 @@
             }
 
         }
-
     </script>
 @endsection
