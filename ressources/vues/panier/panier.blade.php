acc926ce7a1bb1dc2caca4717ae51d4b8344a2c9
Samuel Loranger <samuel@csfnet-016-202.csfoy.ca>
2019-10-22 14:05:06 -0400

    update panier + css

--- ressources/vues/panier/panier.blade.php
+++ ressources/vues/panier/panier.blade.php
@@ -3,37 +3,54 @@
 @section('contenu')
 
     <h1>Panier</h1>
-    <div>
+    <div class="panier row">
         @if(count($elementsPanier) != 0)
-            @foreach($elementsPanier as $item)
-                <div style="background-color:cornflowerblue">
-                    <h2>{{ $item -> livre -> titre }}</h2>
-                    <h3>Par @foreach($item -> livre -> getAuteurs() as $auteur) {{ $auteur->getNomPrenom() }} @endforeach</h3>
-                    <form>
-                        <input type="hidden" name="controleur" value="panier">
-                        <input type="hidden" name="action" value="updateItem">
+            <div class="panier__items col-md-9">
+                @foreach($elementsPanier as $item)
+                    <div class="panier__items__item row">
+                        <div class="infosLivre col-md-5">
+                            <p class="infosLivre__titreLivre">{{ $item -> livre -> titre }}</p>
+                            <p class="infosLivre__auteurLivre">Par @foreach($item -> livre -> getAuteurs() as $auteur) {{ $auteur->getNomPrenom() }} @endforeach</p>
+                        </div>
+                        <div class="formLivre col-md-3">
+                            <form>
+                                <input type="hidden" name="controleur" value="panier">
+                                <input type="hidden" name="action" value="updateItem">
 
-                        <label for="qte">Quantité</label>
-                        <select name="qte">
-                            @for($intCtr = 1; $intCtr <= 10; $intCtr++)
-                                <option @if($item->quantite == $intCtr) selected @endif>{{ $intCtr }}</option>
-                            @endfor
-                        </select>
-                        <input type="hidden" name="isbn" value="{{$item->livre->isbn}}">
+                                <label for="qte">Quantité</label>
+                                <select name="qte">
+                                    <option value="0">0 (supprimer)</option>
+                                    @for($intCtr = 1; $intCtr <= 10; $intCtr++)
+                                        <option value="{{ $intCtr }}" @if($item->quantite == $intCtr) selected @endif>{{ $intCtr }}</option>
+                                    @endfor
+                                </select>
+                                <input type="hidden" name="isbn" value="{{$item->livre->isbn}}">
 
-                        <input id="updatePanier" type="submit" value="Mettre à jour">
-                    </form>
-                    <p><a href="index.php?controleur=panier&action=supprimerItem&isbn={{ $item->livre->isbn }}">Supprimer l'item du panier</a></p>
+                                <input id="updatePanier" type="submit" value="Mettre à jour">
+                            </form>
 
-                    <p>Prix par livre: {{ $item->livre->getPrix() }}</p>
-                    <p>Prix total livre: {{ $item->getMontantTotalFormate() }}</p>
+                            <p><a href="index.php?controleur=panier&action=supprimerItem&isbn={{ $item->livre->isbn }}">Supprimer l'item du panier</a></p>
+                        </div>
+
+                        <div class="prixLivre col-md-4">
+
+                            <p>Prix par livre: {{ $item->livre->getPrix() }}</p>
+                            <p>Prix total livre: {{ $item->getMontantTotalFormate() }}</p>
+                        </div>
+
+                    </div>
+                @endforeach
+            </div>
+            <div class="panier__infosPanier col-md-3">
+                <div class="contenu">
+                    <p>Sous-total: {{ $montantSousTotal }}</p>
+                    <p>Livraison: {{ $fraisLivraison }}</p>
+                    <p>Taxes: {{ $montantTPS }}</p>
+                    <p>Total: {{ $montantTotal }}</p>
+                    <button class="">Payer</button>
                 </div>
-            @endforeach
+            </div>
 
-            <p>Sous-total: {{ $montantSousTotal }}</p>
-            <p>Livraison: {{ $fraisLivraison }}</p>
-            <p>Taxes: {{ $montantTPS }}</p>
-            <p>Total: {{ $montantTotal }}</p>
         @else
             <h2>Le panier est vide.</h2>
         @endif
