c83eb53a1d69fe7975ab288810b0164d5356f317
Olivier Papineau <olivier.12.papineau@gmail.com>
2019-10-29 15:31:13 -0400

    Ajout du modele User et du controleur de compte + vues

---
+++ ressources/vues/compte/connexion.blade.php
@@ -0,0 +1,6 @@
+@extends('gabarit')
+
+@section("contenu")
+    <h3>Ceci est la page de connexion</h3>
+@endsection
+
---
+++ ressources/vues/compte/inscription.blade.php
@@ -0,0 +1,5 @@
+@extends('gabarit')
+
+@section("contenu")
+    <h3>Ceci est la page d'inscription</h3>
+@endsection
