fec9f9a497dd362c043c47df4ab51e36245e6353
Olivier Papineau <olivier.12.papineau@gmail.com>
2019-10-30 15:58:34 -0400

    Ajout de la creation de compte

--- ressources/vues/compte/inscription.blade.php
+++ ressources/vues/compte/inscription.blade.php
@@ -1,5 +1,34 @@
 @extends('gabarit')
 
 @section("contenu")
-    <h3>Ceci est la page d'inscription</h3>
+    <div>
+        <h3>Ceci est la page d'inscription</h3>
+        <form action="index.php?controleur=compte&action=inscrire" method="POST">
+            <label for="prenom">Prenom</label>
+            <input type="text" name="prenom" id="prenom">
+            <br>
+
+            <label for="nom">Nom</label>
+            <input type="text" name="nom" id="nom">
+            <br>
+
+            <label for="email">Courriel</label>
+            <input type="email" name="email" id="email">
+            <br>
+
+            <label for="tel">Numero de telephone</label>
+            <input type="number" name="tel" id="tel">
+            <br>
+
+            <label for="mdp">Mot de passe</label>
+            <input type="password" name="mdp" id="mdp">
+            <br>
+
+            <label for="c_mdp">Confirmer le mot de passe</label>
+            <input type="password" name="c_mdp" id="c_mdp">
+            <br>
+
+            <input class="bouton_panier" type="submit" value="S'inscrire">
+        </form>
+    </div>
 @endsection
