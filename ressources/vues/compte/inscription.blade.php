795aa6e433d0dc69e4dc65fcea2053b8ed6e4fbc
Olivier Papineau <olivier.12.papineau@gmail.com>
2019-11-05 15:23:28 -0500

    Ajout des styles pour la page inscription

--- ressources/vues/compte/inscription.blade.php
+++ ressources/vues/compte/inscription.blade.php
@@ -1,34 +1,52 @@
 @extends('gabarit')
 
 @section("contenu")
-    <div>
-        <h3>Ceci est la page d'inscription</h3>
-        <form action="index.php?controleur=compte&action=inscrire" method="POST">
-            <label for="prenom">Prenom</label>
-            <input type="text" name="prenom" id="prenom">
-            <br>
+    <div class="inscription">
+        <div class="inscription__formulaire bloc_formulaire">
+            <h3 class="inscription__formulaire__titre">S'inscrire a Traces</h3>
+            <form action="index.php?controleur=compte&action=inscrire" method="POST">
+                <label for="prenom">Prenom</label>
+                <input class="inscription__formulaire__champ" type="text" name="prenom" id="prenom">
+                <br>
 
-            <label for="nom">Nom</label>
-            <input type="text" name="nom" id="nom">
-            <br>
+                <label for="nom">Nom</label>
+                <input class="inscription__formulaire__champ" type="text" name="nom" id="nom">
+                <br>
 
-            <label for="email">Courriel</label>
-            <input type="email" name="email" id="email">
-            <br>
+                <label for="email">Courriel</label>
+                <input class="inscription__formulaire__champ" type="email" name="email" id="email">
+                <br>
 
-            <label for="tel">Numero de telephone</label>
-            <input type="number" name="tel" id="tel">
-            <br>
+                <label for="tel">Numero de telephone</label>
+                <small class="inscription__formulaire__consignes">Ex: 418-999-9999</small>
+                <input class="inscription__formulaire__champ" type="number" name="tel" id="tel">
+                <br>
 
-            <label for="mdp">Mot de passe</label>
-            <input type="password" name="mdp" id="mdp">
-            <br>
+                <label for="mdp">Mot de passe</label>
+                <small class="inscription__formulaire__consignes">8 caracteres, lettres et chiffres, une majuscule.</small>
+                <input class="inscription__formulaire__champ" type="password" name="mdp" id="mdp">
+                <br>
 
-            <label for="c_mdp">Confirmer le mot de passe</label>
-            <input type="password" name="c_mdp" id="c_mdp">
-            <br>
+                <div class="inscription__formulaire__afficher_mdp">
+                    <input type="checkbox" name="afficher_mdp" id="afficher_mdp">
+                    <label for="afficher_mdp"></label>
+                    <span>Afficher le mot de passe</span>
+                    <a class="inscription__formulaire__lien" href="#">Mot de passe oublie?</a>
+                </div>
 
-            <input class="bouton_panier" type="submit" value="S'inscrire">
-        </form>
+                <label for="c_mdp">Confirmer le mot de passe</label>
+                <input class="inscription__formulaire__champ" type="password" name="c_mdp" id="c_mdp">
+                <br>
+
+                <input class="bouton_panier" type="submit" value="S'inscrire">
+            </form>
+        </div>
+        <div class="inscription__deja_client bloc_formulaire">
+            <h4>Vous avez deja un compte?</h4>
+            <a class="inscription__formulaire__lien" href="index.php?controleur=compte&action=connexion">Se connecter</a>
+        </div>
+        <div class="inscription__sans_compte bloc_formulaire">
+            <a class="bouton inscription__formulaire__lien" href="#">Acheter sans creer de compte</a>
+        </div>
     </div>
 @endsection
