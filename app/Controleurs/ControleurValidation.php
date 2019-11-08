9354834fa5d52def13ed784b7b52312efdd7de27
Zachary Nicol-P <1735193@cegep-ste-foy.qc.ca>
2019-11-08 12:58:37 -0500

    update

--- app/Controleurs/ControleurValidation.php
+++ app/Controleurs/ControleurValidation.php
@@ -5,21 +5,40 @@ declare(strict_types=1);
 namespace App\Controleurs;
 
 use App\App;
+use App\Modeles\Adresse;
 
 
 class ControleurValidation
 {
     private $blade = null;
+    private $session = null;
 
     public function __construct()
     {
         $this->blade = App::getInstance()->getBlade();
+        $this->session = App::getInstance()->getSession();
     }
 
     public function validation(): void
     {
-        $tDonnees = array("nomPage" => "Validation");
-        $tDonnees = array_merge($tDonnees, ControleurSite::getDonneeFragmentPiedDePage());
+        $arrInfosLivraison = $this->session->getItem("livraison");
+        $arrInfosFacturation = $this->session->getItem("facturation");
+        $arrPanier = $this->session->getItem("panier");
+
+
+
+        $tDonnees = array_merge(
+            ControleurSite::getDonneeFragmentPiedDePage(),
+            array("nomPage" => "Validation"),
+            array("adresse" => $arrInfosLivraison["adresse"]),
+            array("ville" => $arrInfosLivraison["ville"]),
+            array("province" => Adresse::trouverProvince($arrInfosLivraison["abbrProvince"])),
+            array("codePostal" => $arrInfosLivraison["codePostal"]),
+            array("nomComplet" => $arrInfosFacturation["nomComplet"]),
+            array("noCarte" => substr($arrInfosFacturation["noCarte"],-4)),
+            array("dateExpiration" => ($arrInfosFacturation["dateExpirationCarte"])),
+            array("courriel"=> $this->session->getItem("courriel"))
+        );
         echo $this->blade->run("transaction.validation", $tDonnees);
     }
 
--- ressources/vues/transaction/validation.blade.php
+++ ressources/vues/transaction/validation.blade.php
@@ -12,25 +12,60 @@
         <div class="transaction__background">
             <a class="transaction__centrer transaction__bouton"
                href="index.php?controleur=validation&action=confirmation">PASSER LA COMMANDE</a>
-            {{--    À COMPLÉTER AVEC INFOS DE L'UTILISATEUR        --}}
-            <p>Livraison à </p>
+            <p>Livraison à <b>{{$nomComplet}}</b></p>
             <p>Date de livraison estimée :</p>
             <div class="validation__sommaire">
                 <p class="transaction__sous-titre">Sommaire de la commande</p>
             </div>
             <div class="validation__adresseLivraison">
                 <p class="transaction__sous-titre">Adresse de livraison</p>
+                <p>
+                    {{$nomComplet}}<br/>
+                    {{$adresse}}<br/>
+                    {{$ville}}<br/>
+                    {{$province}}<br/>
+                    {{$codePostal}}
+                </p>
+                <button>Modifier</button>
             </div>
             <div class="validation__infoFacturation">
                 <p class="transaction__sous-titre">Informations de facturation</p>
-               <p>Mode de paiement : carte de crédit</p>
+                <p>Mode de paiement : carte de crédit</p>
+                <p>XXXX XXXX XXXX {{$noCarte}}</p>
+                <p>Expiration : {{$dateExpiration}}</p>
                 <button>Modifier</button>
-                <p>Adresse de facturation</p>
+                <p class="transaction__sous-titre">Adresse de facturation</p>
+                <p>
+                    {{$nomComplet}}<br/>
+                    {{$adresse}}<br/>
+                    {{$ville}}<br/>
+                    {{$province}}<br/>
+                    {{$codePostal}}
+                </p>
                 <button>Modifier</button>
-                <p>Informations</p>
+                <p class="transaction__sous-titre">Informations</p>
+                <p>{{$courriel}}</p>
                 <button>Modifier</button>
             </div>
         </div>
         <h2 class="transaction__titre">Mon panier</h2>
+        <p>{{$titre}}</p>
+        <p>{{$auteur}}</p>
+        <p>{{$prix}}</p>
+        <p>Quantité :</p>
+        <select>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+            <option>6</option>
+            <option>7</option>
+            <option>8</option>
+            <option>9</option>
+            <option>10</option>
+        </select>
+    {{--    @foreach($elementsPanier as $item)--}}
 
+    {{--    @endforeach--}}
 @endsection
