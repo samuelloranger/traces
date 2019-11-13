457e2b540bc147d593e1a435d4c59fa671844851
Samuel Loranger <samuelloranger@gmail.com>
2019-11-12 20:58:52 -0500

    finalisation styles confirmation

--- ressources/vues/transaction/confirmation.blade.php
+++ ressources/vues/transaction/confirmation.blade.php
@@ -1,7 +1,7 @@
 @extends('transaction.gabarit')
 
 @section('contenu')
-    <div class="transaction">
+    <div class="confirmation">
         <h1 class="transaction__titre">Confirmation</h1>
         <div class="transaction__centrer">
             <div class="transation__messageGeneral">
@@ -18,20 +18,20 @@
             <div class="confirmation__resumeCommande__zoneInfo">
                 <h3>Sommaire de votre commande</h3>
                 <div class="infoPanierFlex text-right">
-                    <p class="col-sm-6">3 items</p>
-                    <p class="col-sm-6">CAD 68,95$</p>
+                    <p>3 items</p>
+                    <p>CAD 68,95$</p>
                 </div>
                 <div class="infoPanierFlex text-right">
-                    <p class="col-sm-6">TPS 5%</p>
-                    <p class="col-sm-6">CAD 4,45$</p>
+                    <p>TPS 5%</p>
+                    <p>CAD 4,45$</p>
                 </div>
                 <div class="infoPanierFlex text-right">
-                    <p class="col-sm-6">Livraison standard</p>
-                    <p class="col-sm-6">CAD 4,45$</p>
+                    <p>Livraison standard</p>
+                    <p>CAD 4,45$</p>
                 </div>
                 <div class="infoPanierFlex text-right">
-                    <p class="col-sm-6">Total</p>
-                    <p class="col-sm-6">CAD 4,45$</p>
+                    <p>Total</p>
+                    <p>CAD 4,45$</p>
                 </div>
             </div>
 
@@ -57,7 +57,7 @@
                 <h3>Mode de paiement</h3>
                 <p>Nom prénom</p>
 
-                <div class="carte">
+                <div class="mode-paiement">
                     {{--        Ici tu envoie le mode de paiement sélectionné, tu pourras changer la source de l'image ensuite        --}}
                     <img class="logo" src="liaisons/images/transaction/visa.svg" alt="visa">
                     {{--                <img class="logo" src="liaisons/images/transaction/mastercard.svg" alt="Mastercard">--}}
@@ -77,20 +77,36 @@
             {{--      LA BOUCLE D'AFFICHAGE VA ÊTRE ICI      --}}
             <div class="confirmation__resumeItems__item">
                 <div class="conteneurItem">
-                    <img class="couvertureLivre" src="liaisons/images/couvertures-livres/L97828944851871.jpg">
+                    <img class="couvertureLivre" src="liaisons/images/couvertures-livres/L97828944851871_carre.jpg">
                     <div class="titreAuteur">
                         <h4>Les chroniques d'une mère indigne</h4>
                         <p>Caroline Allard</p>
                     </div>
+
+                    <div class="prixItem">
+                        <p>Total (2):</p>
+                        <p>18.95$</p>
+                    </div>
                 </div>
-                <div class="prixItem">
-                    <p>Total:</p>
-                    <p>18.95$</p>
-                 </div>
+            </div>
+
+            <div class="confirmation__resumeItems__item">
+                <div class="conteneurItem">
+                    <img class="couvertureLivre" src="liaisons/images/couvertures-livres/L97828944851871_carre.jpg">
+                    <div class="titreAuteur">
+                        <h4>Les chroniques d'une mère indigne</h4>
+                        <p>Caroline Allard</p>
+                    </div>
+
+                    <div class="prixItem">
+                        <p>Total (2):</p>
+                        <p>18.95$</p>
+                    </div>
+                </div>
             </div>
         </div>
 
         <button class="transaction__centrer transaction__bouton">IMPRIMER LE REÇU DE VOTRE COMMANDE</button>
-        <a class="transaction__centrer lienRetour" href="index.php?controleur=site&action=accueil">CONTINUER À MAGASINER</a>
+        <a class="transaction__centrer lienRetour text-center" href="index.php?controleur=site&action=accueil">CONTINUER À MAGASINER</a>
     </div>
 @endsection
