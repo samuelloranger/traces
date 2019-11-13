6d1e4ae2be63c939a5e403e7f7c581a85ef4a782
Zachary Nicol-P <1735193@cegep-ste-foy.qc.ca>
2019-11-13 14:04:00 -0500

    update confirmation

--- ressources/vues/transaction/confirmation.blade.php
+++ ressources/vues/transaction/confirmation.blade.php
@@ -6,9 +6,9 @@
         <div class="transaction__centrer">
             <div class="transation__messageGeneral">
                 <span class="icone__validation"></span>
-                <h2>Nous avons bien reçu votre commande.</h2>
+                <h2 class="transaction__sous-titre">Nous avons bien reçu votre commande.</h2>
             </div>
-            <p>Elle vous sera expédiée selon les modalitées que vous avez choisies. N'hésitez pas à consulter notre service à la clientèle pour plus d'informations relatives à votre commande ou votre compte.</p>
+            <p class="confirmation__espacement">Elle vous sera expédiée selon les modalitées que vous avez choisies. N'hésitez pas à consulter notre service à la clientèle pour plus d'informations relatives à votre commande ou votre compte.</p>
             <p><b>Votre numéro de confirmation est le:</b></p>
             <p><b>XXXXXXXXXXXXX</b></p>
             <p>Vous recevrez d'ici quelques minutes une confirmation de votre commande par courriel.</p>
@@ -16,8 +16,8 @@
 
         <div class="transaction__background confirmation__resumeCommande">
             <div class="confirmation__resumeCommande__zoneInfo">
-                <h3>Sommaire de votre commande</h3>
-                <div class="infoPanierFlex text-right">
+                <h3 class="transaction__sous-titre">Sommaire de votre commande</h3>
+                <div class="infoPanierFlex text-right confirmation__espacement">
                     <p>3 items</p>
                     <p>CAD 68,95$</p>
                 </div>
@@ -36,8 +36,8 @@
             </div>
 
             <div class="confirmation__resumeCommande__zoneInfo">
-                <h3>Adresse de livraison</h3>
-                <p>Prénom nom</p>
+                <h3 class="transaction__sous-titre">Adresse de livraison</h3>
+                <p class="confirmation__espacement">Prénom nom</p>
                 <p>133 Rue Sieur Nicol</p>
                 <p>Québec</p>
                 <p>QC</p>
@@ -45,8 +45,8 @@
             </div>
 
             <div class="confirmation__resumeCommande__zoneInfo">
-                <h3>Adresse de facturation</h3>
-                <p>Prénom nom</p>
+                <h3 class="transaction__sous-titre">Adresse de facturation</h3>
+                <p class="confirmation__espacement">Prénom nom</p>
                 <p>133 Rue Sieur Nicol</p>
                 <p>Québec</p>
                 <p>QC</p>
@@ -54,8 +54,8 @@
             </div>
 
             <div class="confirmation__resumeCommande__zoneInfo">
-                <h3>Mode de paiement</h3>
-                <p>Nom prénom</p>
+                <h3 class="transaction__sous-titre">Mode de paiement</h3>
+                <p class="confirmation__espacement">Nom prénom</p>
 
                 <div class="mode-paiement">
                     {{--        Ici tu envoie le mode de paiement sélectionné, tu pourras changer la source de l'image ensuite        --}}
@@ -67,8 +67,8 @@
             </div>
 
             <div class="transaction__resumePanier zone">
-                <h3>Informations</h3>
-                <p>samuelloranger@gmail.com</p>
+                <h3 class="transaction__sous-titre">Informations</h3>
+                <p class="confirmation__espacement">samuelloranger@gmail.com</p>
                 <p>581-748-0646</p>
             </div>
         </div>
