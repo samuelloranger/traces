999e7dafbf023c83723835a98c489c23c3d41d5f
Zachary Nicol-P <1735193@cegep-ste-foy.qc.ca>
2019-11-15 10:31:23 -0500

    update

--- app/Modeles/Categories.php
+++ app/Modeles/Categories.php
@@ -7,37 +7,63 @@ use App\App;
 use App\ConnexionBD;
 use \PDO;
 
-class Categories{
+class Categories
+{
     //Attributs
     private $pdo = null;
     public $id = "";
     public $nom_fr = "";
     public $nom_en = "";
 
-    public function __construct(){
+    public function __construct()
+    {
 
     }
 
-    public static function trouverTout():array{
-        $pdo = App::getInstance() -> getPDO();
+    public static function trouverTout(): array
+    {
+        $pdo = App::getInstance()->getPDO();
 
         //Requête SQL
         $sql = "SELECT * FROM categories";
 
         //On prépare la requête
-        $requetePreparee = $pdo -> prepare($sql);
+        $requetePreparee = $pdo->prepare($sql);
 
         // Définir le mode de récupération
-        $requetePreparee -> setFetchMode(PDO::FETCH_CLASS, Categories::class);
+        $requetePreparee->setFetchMode(PDO::FETCH_CLASS, Categories::class);
 
         // Exécuter la requête
-        $requetePreparee -> execute();
+        $requetePreparee->execute();
 
         // Récupérer une seule occurrence à la fois
-        $arrCategories = $requetePreparee -> fetchAll();
+        $arrCategories = $requetePreparee->fetchAll();
 
         return $arrCategories;
     }
 
+    public static function trouverParId($id_categorie): Categories
+    {
+        $pdo = App::getInstance()->getPDO();
+
+        //Requête SQL
+        $sql = "SELECT * FROM categories WHERE id = :id_categorie";
+
+        $requetePreparee = $pdo->prepare($sql);
+
+        $requetePreparee->bindParam(":id_categorie", $id_categorie, PDO::PARAM_INT);
+        // Définir le mode de récupération
+        $requetePreparee->setFetchMode(PDO::FETCH_CLASS, Categories::class);
+
+        // Exécuter la requête
+        $requetePreparee->execute();
+
+        // Récupérer une seule occurrence à la fois
+        $categorie = $requetePreparee->fetch();
+
+        return $categorie;
+    }
+
 }
+
 ?>
